3261054ad3b17062c2d7e0887c237e1188fa0d78
Nokome Bentley <nokome@stenci.la>
2022-09-08 18:53:20 +1200

    fix(Build): Add `unoptimized: true` option to Next.js config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const nextConfig = {
     scrollRestoration: true,
     images: {
       allowFutureImage: true,
+      // Setting to avoid this error:
+      //   Error: Image Optimization using Next.js' default loader is not compatible with `next export`.
+      //   Possible solutions:
+      //     - Use `next start` to run a server, which includes the Image Optimization API.
+      //     - Configure `images.unoptimized = true` in `next.config.js` to disable the Image Optimization API.
+      unoptimized: true
     },
   },
 }
